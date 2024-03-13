11b70ca07af0601bf52d91a647bd5b6ade3a94b0
therohann <158451267+therohann@users.noreply.github.com>
2024-03-13 06:05:15 +0000

    changes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,15 +1,15 @@
 const accountId = 144553
-let accountEmail = "hitesh@google.com"
+let accountEmail = "rohanshaik234@gmail.com"
 var accountPassword = "12345"
-accountCity = "Jaipur"
+accountCity = "Hyderabad"
 let accountState;
 
 // accountId = 2 // not allowed
 
 
-accountEmail = "hc@hc.com"
+accountEmail = "shaikrohan234@gmail.com"
 accountPassword = "21212121"
-accountCity = "Bengaluru"
+accountCity = "Rangareddy"
 
 console.log(accountId);
 
